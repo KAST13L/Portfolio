ba98a298d7128d0a910bdb89eb83eb2221eddd16
KAST13L <o4enbsmeshno@gmail.com>
2022-10-26 15:32:03 +0300

    feat: validation on contacts form

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -12,11 +12,17 @@ type ContactsPropsType = {
 
 export function Contacts(props: ContactsPropsType) {
 
-    const { register, handleSubmit, ...form } = useForm();
+    const { register, handleSubmit, formState:{errors},...form } = useForm({
+        defaultValues:{
+            name:'',
+            email:'',
+            message:''
+        }
+    });
+
     const [isDisabled, setIsDisabled] = useState<boolean>(false)
     const onSubmit = async (data: any) => {
         setIsDisabled(true)
-        // http://localhost:3001/sendMessage
         try {
             const res = await axios.post('https://gmail-nodej.herokuapp.com/sendMessage', data)
             alert('Message is sending!')
@@ -45,15 +51,25 @@ export function Contacts(props: ContactsPropsType) {
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div>
                                 <label>{props.contactsComponent.name}</label>
-                                <input {...register("name")} />
+                                <input {...register("name", {
+                                    required: "Name is required"
+                                })} />
                             </div>
                             <div>
                                 <label>{props.contactsComponent.email}</label>
-                                <input {...register("email")}/>
+                                <input {...register('email', {
+                                    required: 'Email is required',
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: 'Write correct email'
+                                    }
+                                })}/>
                             </div>
                             <div>
                                 <label>{props.contactsComponent.formMessage}</label>
-                                <textarea {...register("message")} ></textarea>
+                                <textarea {...register("message", {
+                                    required: 'Message is required'
+                                })} ></textarea>
                             </div>
                             <div>
                                 <button disabled={isDisabled} style={{width:'170px'}}  type={'submit'}>{ isDisabled ? 'loading...' : props.contactsComponent.button} &#9658;</button>
